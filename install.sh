aa33c589dba921bf71e7dd8bfcccc46736fea5b6
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2022-08-10 00:07:39 +0430

    Some changes

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ read TLD
 
 find . -type f | xargs sed -i  's/example/'$domainName'/g'
 find . -type f | xargs sed -i  's/'$domainName.com'/'$domainName.$TLD'/g'
-mv -rf examplehelper {$domainName}helpers
+mv -f examplehelper {$domainName}helpers
 mv example.com-http $domainName.$TLD-http
 mv example.com-https $domainName.$TLD-https
