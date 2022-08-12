84a8ae97ca51e3ceddbd0b3d4c9ff01e8175f342
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2022-08-12 06:27:52 +0430

    Some changes

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ read domainName
 echo "please enter your the top level domain (TLD):(eg: com)"
 read TLD
 
-find . -type f | xargs sed -i  's/example/'$domainName'/g'
-find . -type f | xargs sed -i  's/'$domainName.com'/'$domainName.$TLD'/g'
-mv -f examplehelper ${domainName}helper
-mv example.com-http $domainName.$TLD-http
-mv example.com-https $domainName.$TLD-https
+sudo find . -type f | xargs sed -i  's/example/'$domainName'/g'
+sudo find . -type f | xargs sed -i  's/'$domainName.com'/'$domainName.$TLD'/g'
+sudo mv -f examplehelper ${domainName}helper
+sudo mv example.com-http $domainName.$TLD-http
+sudo mv example.com-https $domainName.$TLD-https
